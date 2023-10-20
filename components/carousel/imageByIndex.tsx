03f0565d7b1ce135a2df9056eb41f0cc4abf538f
shander3377 <agrim310108@gmail.com>
2023-10-20 19:57:49 +0530

    files deleted
    asdasda

--- components/carousel/imageByIndex.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import image1 from './images/slide-1.jpeg'
-import image2 from './images/slide-2.jpeg'
-import { StaticImageData } from "next/image"
-export const images: StaticImageData[] = [image1, image2]
-
-const imageByIndex = (index: number): StaticImageData => images[index % images.length]
-console.log(imageByIndex)
-export default imageByIndex
