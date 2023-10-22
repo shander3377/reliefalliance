f850500a4493908dc3641b9f6380a34c8f3e3d51
EreN <145914846+EreN4710A@users.noreply.github.com>
2023-10-22 08:01:11 +0530

    chote mote color wale commits

--- app/page.tsx
+++ app/page.tsx
@@ -123,10 +123,10 @@ export default function Home() {
 				<div className="gap-2 grid grid-cols-12 grid-rows-2 ">
 					<Card className="col-span-12 sm:col-span-4 h-[300px]">
 						<CardHeader className="absolute z-10 top-1 flex-col !items-start">
-							<p className="text-tiny text-green-600 uppercase font-bold">
+							<p className="text-tiny text-black/100 uppercase font-bold">
 								Live Articles
 							</p>
-							<h4 className="text-green-600 font-medium text-large">
+							<h4 className="text-black/100 font-medium text-large">
 								Surf through the live info
 							</h4>
 						</CardHeader>
