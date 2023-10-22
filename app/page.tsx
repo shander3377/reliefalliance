83cedd986789291bfdcac45f14814528112820f4
EreN <145914846+EreN4710A@users.noreply.github.com>
2023-10-22 07:33:48 +0530

    color test

--- app/page.tsx
+++ app/page.tsx
@@ -174,8 +174,8 @@ export default function Home() {
 						className="w-full h-[300px] col-span-12 sm:col-span-5"
 					>
 						<CardHeader className="absolute z-10 top-1 flex-col items-start">
-							<p className="text-tiny text-black/60 uppercase font-bold">Latest war updates</p>
-							<h4 className="text-black font-medium text-2xl">Join us now</h4>
+							<p className="text-tiny text-blue-600 uppercase font-bold">Latest war updates</p>
+							<h4 className="text-blue-600 font-medium text-2xl">Join us now</h4>
 						</CardHeader>
 						<Image
 							removeWrapper
