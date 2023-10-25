e07e694fe4640dce4d2cb06f1e2429c1e7657350
bhunveshjoshi <bhuveshjoshi139@gmail.com>
2023-10-25 11:29:15 +0530

    changess
    fdsfsdfs

--- app/agencyDashboard/page.tsx
+++ app/agencyDashboard/page.tsx
@@ -15,11 +15,18 @@ import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
 import { Divider } from "@nextui-org/divider";
 import { useRouter } from "next/navigation";
 import React from "react";
+import addData from "@/firebase/firestore/addData";
 
+import { Tooltip } from "@nextui-org/tooltip";
 import { db } from "../../config/firebase.config";
 import { Input } from "@nextui-org/input";
 import { MailIcon } from "@/components/icons";
 import AgencyRegisterModal from "@/components/agencyRegisterModal";
+import {EditIcon} from "./EditIcon";
+import {DeleteIcon} from "./DeleteIcon";
+import {EyeIcon} from "./EyeIcon";
+import {columns, users} from "./data";
+import { Chip, ChipProps } from "@nextui-org/chip";
 import {
 	getFirestore,
 	collection,
@@ -28,7 +35,9 @@ import {
 	onSnapshot,
 	updateDoc,
 	doc,
-	getDocs
+	getDocs, 	
+	setDoc,
+	addDoc
 } from "firebase/firestore";
 import {
 	Modal,
@@ -38,6 +47,22 @@ import {
 	ModalFooter,
 	useDisclosure,
 } from "@nextui-org/modal";
+
+import { Table,
+	TableHeader,
+	TableColumn,
+	TableBody,
+	TableRow,
+	TableCell} from "@nextui-org/table"
+import {Tabs, Tab} from "@nextui-org/tabs";
+
+const statusColorMap: Record<string, ChipProps["color"]>  = {
+	active: "success",
+	paused: "danger",
+	vacation: "warning",
+  };
+  
+  type Userr = typeof users[0];
 export default function AgencyDashboardPage() {
 	type userDoc = {
 		name: string;
@@ -48,36 +73,200 @@ export default function AgencyDashboardPage() {
 	};
 	const { user } = useAuthContext();
 	const router = useRouter();
-	const [open, setOpen] = React.useState(false);
-	const [open2, setOpen2] = React.useState(false);
+	const [title, setTitle] = React.useState("");
+	const [description, setDescription] = React.useState("");
+	const [location, setLocation] = React.useState("");
 
 	const [agency, setAgency] = React.useState<string>();
 	const [docc, setDocc] = React.useState<userDoc>();
 	const [docId, setDocId] = React.useState<string>();
+	async function createCrisis(){
+var data = {
+	title: title,
+	description: description,
+	location: location,
+	agencies: [docc?.agency]
+}
+var colref = collection(db, "crisis")
+var hehe = await addDoc(colref, data);
+var id = hehe.id
+console.log(id)
+const userDoc = doc(collection(db, "users"), docId);
+						updateDoc(userDoc, {
+							crisis: true,
+							crisisId: id
 
+						});
+console.log(docc?.agency)
+var q = query(collection(db, "agencies"), where("agencyCode", "==", docc?.agency));
+var snapshot = await getDocs(q)
+	snapshot.forEach(async(docu: any) => {
+		
+						const agencyDoc = doc(collection(db, "agencies"), docu.id);
+						updateDoc(agencyDoc, {
+							crisis: true,
+							crisisId: id
+						});
+					})
+	}
+const [disabledKey, setDisabledKey]  = React.useState("create")
 	React.useEffect(() => {
+		async function getData()
+{
+	var q = query(collection(db, "users"), where("email", "==", user.email));
+	var snapshot = await getDocs(q)
+		snapshot.forEach(async(docu: any) => {
+			
+			setDocc(await docu.data())
+			setDocId(docu.id)
+			if(docu.data()){
+			if(docu.data().agency === "000000"){
+router.push("/agency")
+			} else {
+console.log(docc)
+
+				if(docu.data().head){
+setDisabledKey("")
+				}
+			}
+		}
+		});
+}	
 		console.log(user);
 		if (user.email == null) {
 			console.log("should go");
 			router.push("/");
 		} else {
-			const checkAgency = async function (): Promise<any> { 
-
-			var q = query(collection(db, "users"), where("email", "==", user.email));
-			var snapshot = await getDocs(q)
-					snapshot.forEach(async (docu: any) => {
-						var agencyCode = await docu.data().agency
-						if(agencyCode === "000000"){
-							console.log("should")
-							return router.push("/agency")
-						} 
-					})		
-				}
-				checkAgency()
+			getData()
 		}
 	}, [user]);
-
+	const renderCell = React.useCallback((userr: Userr, columnKey: React.Key) => {
+		const cellValue = userr[columnKey as keyof Userr];
+	
+		switch (columnKey) {
+		  case "name":
+			return (
+			  <h1>
+				
+			  
+				{user.email}
+			  </h1>
+			);
+		  case "role":
+			return (
+			  <div className="flex flex-col">
+				<p className="text-bold text-sm capitalize">{cellValue}</p>
+				<p className="text-bold text-sm capitalize text-default-400">{user.team}</p>
+			  </div>
+			);
+		  case "status":
+			return (
+			  <Chip className="capitalize" color={statusColorMap[user.status]} size="sm" variant="flat">
+				{cellValue}
+			  </Chip>
+			);
+		  case "actions":
+			return (
+			  <div className="relative flex items-center gap-2">
+				<Tooltip content="Details">
+				  <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+					<EyeIcon />
+				  </span>
+				</Tooltip>
+				<Tooltip content="Edit user">
+				  <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+					<EditIcon />
+				  </span>
+				</Tooltip>
+				<Tooltip color="danger" content="Delete user">
+				  <span className="text-lg text-danger cursor-pointer active:opacity-50">
+					<DeleteIcon />
+				  </span>
+				</Tooltip>
+			  </div>
+			);
+		  default:
+			return cellValue;
+		}
+	  }, []);
 	return(
-		<h1>agency dashbaord</h1>
+		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+		<div className=" inline-block inlinemax-w-lg text-center justify-center">
+		<Tabs key="windows" variant="bordered" aria-label="Tabs variants" color="primary" disabledKeys={[disabledKey]}>
+		<Tab title="All Crises" key="all">
+		<Table aria-label="Example table with custom cells">
+      <TableHeader columns={columns}>
+        {(column) => (
+          <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
+            {column.name}
+          </TableColumn>
+        )}
+      </TableHeader>
+      <TableBody items={users}>
+        {(item) => (
+          <TableRow key={item.id}>
+            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+		</Tab>
+		<Tab title="Neighboring Crises" key="near"/>
+		<Tab title="Create Cisis" key="create">
+			<div>
+		<Input
+						autoFocus
+					
+						label="Title"
+						placeholder="Enter crisis title"
+						variant="bordered"
+						onValueChange={(value: string) => {
+							setTitle(value);
+						}}
+					/>
+					<Input
+						variant="bordered"
+						className="mt-2"
+						label="Location"
+						placeholder="Enter crisis location"
+						onValueChange={(value: string) => {
+							setLocation(value);
+						}}
+					/>
+					<Input
+						variant="bordered"
+						className="mt-2"
+						label="Description"
+						placeholder="Enter crisis location"
+						onValueChange={(value: string) => {
+							setDescription(value);
+						}}
+					/>
+						<Button
+						color="primary"
+						className="object-right-top mt-5"
+						onPress={() => {
+							if (
+								title.length > 2 &&
+								title.length<8 &&
+								description.length > 2 &&
+								description.length<20 &&
+								location.length > 6 
+							) {
+								console.log("sabh valid hai");
+								createCrisis();
+							} else {
+								return alert("Please fill in all the details, title must be less than 8 characters, description less than 20 characters.");
+							}
+						}}
+					>
+						Register Crisis
+					</Button>
+					</div>
+				
+		</Tab>
+	  </Tabs>
+	  </div>
+	  </section>
 	)
 }
