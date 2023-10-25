da3e4e539de693458ddb803ad51ec7100cca5dd6
Erennn <145914846+EreN4710A@users.noreply.github.com>
2023-10-25 09:55:49 +0530

    home page name change

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -249,7 +249,7 @@ export const Navbar = () => {
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-1" href="/">
 						<Logo />
-						<p className="font-bold text-inherit">Disaster</p>
+						<p className="font-bold text-inherit">ReliefAlliance</p>
 					</NextLink>
 				</NavbarBrand>
 				<ul className="hidden lg:flex gap-4 justify-start ml-2">
