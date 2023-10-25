0615c225381e4d590791d857b5283a37fc38b0de
Erennn <145914846+EreN4710A@users.noreply.github.com>
2023-10-25 09:23:03 +0530

    donation page done lesgow

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -59,36 +59,29 @@ return (
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
+              <ModalHeader className="flex flex-col gap-1">Donation</ModalHeader>
               <ModalBody>
                 <Input
                   autoFocus
                   endContent={
                     <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
                   }
-                  label="Email"
-                  placeholder="Enter your email"
+                  label="Item"
+                  placeholder="Item for donation"
                   variant="bordered"
                 />
                 <Input
                   endContent={
                     <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
                   }
-                  label="Password"
-                  placeholder="Enter your password"
-                  type="password"
+                  label="Location"
+                  placeholder="Adress"
                   variant="bordered"
                 />
                 <div className="flex py-2 px-1 justify-between">
-                  <Checkbox
-                    classNames={{
-                      label: "text-small",
-                    }}
-                  >
-                    Remember me
-                  </Checkbox>
+               
                   <Link color="primary" href="#" size="sm">
-                    Forgot password?
+                    About This Feature
                   </Link>
                 </div>
               </ModalBody>
@@ -97,7 +90,7 @@ return (
                   Close
                 </Button>
                 <Button color="primary" onPress={onClose}>
-                  Sign in
+                  Donate
                 </Button>
               </ModalFooter>
             </>
